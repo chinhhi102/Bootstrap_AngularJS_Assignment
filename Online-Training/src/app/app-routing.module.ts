6a310095e47fc52212abdbcd502606863fe1806b
Cong Chinh <chinhhi102@gmail.com>
2019-07-09 17:04:35 +0700

    add Nav

--- Online-Training/src/app/app-routing.module.ts
+++ Online-Training/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './View/Manager_User/login/login.component';
 import { SignupComponent } from './View/Manager_User/signup/signup.component';
+import { GioiThieuComponent } from './View/Common_Page/gioi-thieu/gioi-thieu.component';
+import { LienHeComponent } from './View/Common_Page/lien-he/lien-he.component';
+import { GopYComponent } from './View/Common_Page/gop-y/gop-y.component';
+import { HoiDapComponent } from './View/Common_Page/hoi-dap/hoi-dap.component';
 
 const routes: Routes = [
-  {
-    path: 'login', 
-    component: LoginComponent,
-  }
+  { path: 'login', component: LoginComponent },
+  { path: 'gioithieu', component: GioiThieuComponent },
+  { path: 'lienhe', component: LienHeComponent },
+  { path: 'gopy', component: GopYComponent },
+  { path: 'hoidap', component: HoiDapComponent }
 ];
 
 @NgModule({
